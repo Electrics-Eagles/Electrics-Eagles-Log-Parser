a031422a023a92f62a0f0f20bb1643224f450f4a
Alex Zaslavskis <sahsariga111@gmail.com>
2022-01-28 23:53:24 +0000

    added the finall editions. install_deps.sh is fetching jquery and ploty

---
+++ install_deps.sh
@@ -0,0 +1,4 @@
+wget https://cdn.plot.ly/plotly-2.4.2.min.js 
+wget https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+cp plotly-2.4.2.min.js   ./src-tauri/dev/plotly.js
+cp jquery.min.js  ./src-tauri/dev/jquery.js
