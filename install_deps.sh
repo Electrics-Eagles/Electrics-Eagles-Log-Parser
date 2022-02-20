852149f9c59dab40d0d9a54123dc8b7d04a5f2c8
alex5250 <sahsariga111@gmail.com>
2022-02-20 02:43:34 +0200

    Added  th resie and fixed bullets. Removed rubbish from styles.css

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,8 @@
-wget https://cdn.plot.ly/plotly-2.4.2.min.js 
+wget https://cdn.plot.ly/plotly-2.4.2.min.js
 wget https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js
+wget https://raw.githubusercontent.com/marcj/css-element-queries/master/src/ResizeSensor.js
+wget https://raw.githubusercontent.com/marcj/css-element-queries/master/src/ElementQueries.js
 cp plotly-2.4.2.min.js   ./src-tauri/dev/plotly.js
 cp jquery.min.js  ./src-tauri/dev/jquery.js
+cp ResizeSensor.js ./src-tauri/dev/ResizeSensor.js
+cp ElementQueries.js ./src-tauri/dev/ElementQueries.js
